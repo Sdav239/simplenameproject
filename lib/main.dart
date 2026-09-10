972b629f984b6f1e9b51cec95f91199c29bdc3e6
Sdav239 <Sdav239@lsu.edu>
2026-09-10 16:09:20 -0500

    made it big but not too big

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             const Text('Skylar pushed the button this many times:',
               style: TextStyle(
-                fontSize: 124.0, // Adjust this number to make it bigger
+                fontSize: 34.0, // Adjust this number to make it bigger
                 fontWeight: FontWeight.bold, // Optional: makes it bold too
               ),
             ),
